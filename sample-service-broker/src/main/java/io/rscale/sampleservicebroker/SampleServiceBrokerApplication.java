fd953e9daf355f0ee5cc617f4f049001768688ea
Steve Greenberg <spgreenberg@gmail.com>
2019-01-09 18:01:31 -0700

    initial simple service broker

---
+++ sample-service-broker/src/main/java/io/rscale/sampleservicebroker/SampleServiceBrokerApplication.java
@@ -0,0 +1,14 @@
+package io.rscale.sampleservicebroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleServiceBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleServiceBrokerApplication.class, args);
+	}
+
+}
+
---
+++ sample-service-broker/src/test/java/io/rscale/sampleservicebroker/SampleServiceBrokerApplicationTests.java
@@ -0,0 +1,17 @@
+package io.rscale.sampleservicebroker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleServiceBrokerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
