1900fda5e5ea1a3b272c07e86b87a4b8fd715e73
Steve Greenberg <spgreenberg@gmail.com>
2019-01-10 17:26:04 -0700

    correct manifest path

--- simple-service-broker/ci/tasks/deploy.sh
+++ simple-service-broker/ci/tasks/deploy.sh
@@ -7,5 +7,5 @@ cf login -a $CF_API -u $CF_USER -p $CF_PWD -o $CF_ORG -s $CF_SPACE
 
 pushd cg-customer-broker/simple-service-broker/app-source
   ./gradlew assemble
-  cf push -f simple-service-broker_manifest.yml -p build/libs/*.jar
+  cf push -f ../simple-service-broker_manifest.yml -p build/libs/*.jar
 popd
