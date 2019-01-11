efdb789ec835c56e98571f10336515c387640318
Steve Greenberg <spgreenberg@gmail.com>
2019-01-10 17:16:31 -0700

    correct source path

--- simple-service-broker/ci/tasks/deploy.sh
+++ simple-service-broker/ci/tasks/deploy.sh
@@ -5,8 +5,7 @@ set -e
 
 cf login -a $CF_API -u $CF_USER -p $CF_PWD -o $CF_ORG -s $CF_SPACE
 
-cd cg-customer-broker
-
-./gradlew assemble
-
-cf push -f simple-service-broker_manifest.yml -p build/libs/*.jar
+pushd cg-customer-broker/simple-service-broker/app-source
+  ./gradlew assemble
+  cf push -f simple-service-broker_manifest.yml -p build/libs/*.jar
+popd
