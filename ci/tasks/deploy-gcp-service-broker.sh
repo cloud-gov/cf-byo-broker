8c672e50b19abad1cec57e8f4a9917d690c33f81
Steve Wall <steve.wall@primetimesoftware.com>
2019-01-14 15:09:07 -0700

    wip: Add GCP Service Broker portion of pipeline

--- ci/tasks/deploy-gcp-service-broker.sh
+++ ci/tasks/deploy-gcp-service-broker.sh
@@ -8,7 +8,26 @@ set -e
 cd gcp-service-broker
 
 cat << EOF > manifest.yml
-stuff
+---
+applications:
+  - name: gcp-service-broker
+    product_version: "4.1.0"
+    metadata_version: "1.0"
+    label: 'GCP Service Broker'
+    description: 'A service broker for Google Cloud Platform services.'
+    memory: 1G
+    buildpacks: 
+      - go_buildpack
+    env:
+      GOPACKAGENAME: github.com/GoogleCloudPlatform/gcp-service-broker
+      GOVERSION: go1.10
+      ROOT_SERVICE_ACCOUNT_JSON: $ROOT_SERVICE_ACCOUNT_JSON
+      SECURITY_USER_NAME: secuser
+      SECURITY_USER_PASSWORD: secpass
+      DB_HOST: 104.197.57.63
+      DB_USERNAME: root
+      DB_PASSWORD: micropdb
+
 EOF
 
 cat manifest.yml
