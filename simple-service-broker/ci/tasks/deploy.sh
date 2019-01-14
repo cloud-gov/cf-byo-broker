c1f3442364915f85c6324020381f59c7faa20c74
Steve Greenberg <spgreenberg@gmail.com>
2019-01-14 16:36:37 -0700

    switch broker to go from spring

--- simple-service-broker/ci/tasks/deploy.sh
+++ simple-service-broker/ci/tasks/deploy.sh
@@ -5,7 +5,6 @@ set -e
 
 . cg-customer-broker/ci/tasks/common.sh
 
-pushd cg-customer-broker/simple-service-broker/app-source
-  ./gradlew assemble
-  cf push -f ../simple-service-broker_manifest.yml -p build/libs/*.jar
+pushd broker-source
+  cf push --random-route
 popd
---
+++ simple-service-broker/ci/tasks/marketplace.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. cg-customer-broker/ci/tasks/common.sh
+
+offering_name=`cf m | grep ${BROKER_SERVICE_NAME}`
+
+if [ -z $offering_name ]; then
+  echo "Could not find service offering via 'cf m': Service offering name = ${BROKER_SERVICE_NAME}"
+  exit 1
+fi
+
+plan_name=`cf m | grep ${BROKER_PLAN_NAME}`
+
+if [ -z $plan_name ]; then
+  echo "Could not find service plan via 'cf m': Service plan name = ${BROKER_PLAN_NAME}"
+  exit 1
+fi
