4d38af088825f0630952f124d7c13d7a9694233c
Steve Wall <steve.wall@primetimesoftware.com>
2019-01-14 18:52:57 -0700

    wip: Add the creation of a GCP service broker to the pipeline.

--- ci/tasks/gcp-service-broker-tutorial/create-service-broker.sh
+++ ci/tasks/gcp-service-broker-tutorial/create-service-broker.sh
@@ -5,6 +5,6 @@ set -e
 
 . cg-customer-broker/ci/tasks/common.sh
 
-get_route $GCP_BROKER_APP_NAME
+get_route $GCP_SERVICE_BROKER_APP_NAME
 
 cf csb $GCP_SERVICE_BROKER_NAME $SECURITY_USER_NAME $SECURITY_USER_PASSWORD https://$route --space-scoped
