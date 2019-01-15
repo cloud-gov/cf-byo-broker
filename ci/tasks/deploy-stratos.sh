c956d14462f290ff0258c78a2c9d17ecde759bf9
Steve Greenberg <spgreenberg@gmail.com>
2019-01-14 17:35:51 -0700

    re-add STRATOS_APPNAME

--- ci/tasks/deploy-stratos.sh
+++ ci/tasks/deploy-stratos.sh
@@ -7,4 +7,4 @@ cf login -a $CF_API -u $CF_USER -p $CF_PWD -o $CF_ORG -s $CF_SPACE
 
 cd stratos
 ./build/store-git-metadata.sh
-cf push stratos --random-route
+cf push ${STRATOS_APPNAME} --random-route
