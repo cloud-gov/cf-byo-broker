9a40f47d42ea2f1200b80cad2b7fbd588c3272a1
Steve Wall <steve.wall@primetimesoftware.com>
2019-01-14 19:10:38 -0700

    wip: Add the creation of a GCP service broker to the pipeline.

--- ci/tasks/gcp-service-broker-tutorial/cleanup.sh
+++ ci/tasks/gcp-service-broker-tutorial/cleanup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+#########################################################################
+#
+#  This script performs the cleanup activities for the GCP Service Broker
+#  tutorial pipeline. The intent is to leave the environment in the same
+#  state prior to the execution of the pipeline.
+#
+#########################################################################
 
 set +x
 set -e
 
 . cg-customer-broker/ci/tasks/common.sh
 
-cf d -f $GCP_BROKER_APP_NAME  || echo "App could not be deleted: ${BROKER_APPNAME}"
+# Delete the service broker and then the application backing the service broker.
+cf delete-service-broker $GCP_SERVICE_BROKER_NAME || echo "App could not be deleted: ${GCP_SERVICE_BROKER_NAME}"
+cf d -f $GCP_SERVICE_BROKER_APP_NAME  || echo "App could not be deleted: ${GCP_SERVICE_BROKER_APP_NAME}"
