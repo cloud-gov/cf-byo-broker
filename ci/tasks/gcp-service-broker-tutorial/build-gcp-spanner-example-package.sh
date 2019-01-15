9ea1850520c2c9115a36c5be0876dd07e65d8f52
Steve Wall <steve.wall@primetimesoftware.com>
2019-01-15 08:30:41 -0700

    wip: Create pipeline to build GCP Spanner example app

--- ci/tasks/gcp-service-broker-tutorial/build-gcp-spanner-example-package.sh
+++ ci/tasks/gcp-service-broker-tutorial/build-gcp-spanner-example-package.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+#########################################################################
+#
+#  This script builds the GCP Spanner tutorial application and creates
+#  a zip file with the resulting jar and a Cloud Foundry manifest.yml.
+#  The zip is then stored in an S3 bucket.
+#
+#########################################################################
 
-echo "hello package"
+set +x
+set -e
+
+cd gcp-spanner-example
+
+./mvnw package
