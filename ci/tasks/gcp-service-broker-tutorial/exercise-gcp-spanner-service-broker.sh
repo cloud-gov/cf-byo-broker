0cfa8c69cad32d3e48a8be10377f0a28280b1936
Steve Wall <steve.wall@primetimesoftware.com>
2019-01-15 16:16:26 -0700

    wip: Add task to exercise the GCP service broker

---
+++ ci/tasks/gcp-service-broker-tutorial/exercise-gcp-spanner-service-broker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+###################################################################################
+#
+#  This script exercises the GCP Spanner service create in a previous step.
+#      * Create the Spanner service
+#      * Push an example Spanner application to CF
+#      * Bind the service to the app
+#      * Start the service
+#      * Access the app endpoint and verify the expected results are return.
+#
+####################################################################################
+
+set +x
+set -e
+
+. cg-customer-broker/ci/tasks/common.sh
+
+cf cs $GCP_SERVICE_BROKER_NAME sandbox $GCP_SPANNER_SERVICE_NAME -c `{"name":"trades"}`
+
