3f140b1065e70f2a0ea0b320998c128055239184
Steve Greenberg <spgreenberg@gmail.com>
2019-01-14 18:10:02 -0700

    move stratos to simple-broker

--- ci/tasks/delete-stratos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set +x
-set -e
-
-cf login -a $CF_API -u $CF_USER -p $CF_PWD -o $CF_ORG -s $CF_SPACE
-
-cf delete -f -r $STRATOS_APPNAME || echo "${STRATOS_APPNAME} could not be deleted"
