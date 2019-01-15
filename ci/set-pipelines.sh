3e40a307ffa1e03ba61e11edece11b2442928fda
Steve Greenberg <spgreenberg@gmail.com>
2019-01-14 17:30:52 -0700

    remove unused pipeline now included in simple-service-broker

--- ci/set-pipelines.sh
+++ ci/set-pipelines.sh
@@ -12,6 +12,4 @@ this_directory=`dirname "$0"`
 
 fly -t ${target} set-pipeline -p simple-service-broker-tutorial -c ${this_directory}/../simple-service-broker/ci/pipeline.yml
 
-fly -t ${target} set-pipeline -p stratos-in-dev -c ${this_directory}/stratos-pipeline.yml
-
 fly -t ${target} set-pipeline -p gcp-spanner-broker-tutorial -c ${this_directory}/gcp-spanner-broker-tutorial.yml
