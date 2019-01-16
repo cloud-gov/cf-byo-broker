e40b0b8529594d981342e16d222fec7e5df3cfab
Steve Wall <steve.wall@primetimesoftware.com>
2019-01-16 08:51:52 -0700

    wip: Add task to exercise the GCP service brokerush

--- ci/tasks/gcp-service-broker-tutorial/exercise-gcp-spanner-service-broker.sh
+++ ci/tasks/gcp-service-broker-tutorial/exercise-gcp-spanner-service-broker.sh
@@ -15,5 +15,17 @@ set -e
 
 . cg-customer-broker/ci/tasks/common.sh
 
+# Start the creation of the service instance
 cf cs $GCP_SPANNER_SERVICE $GCP_SPANNER_SERVICE_PLAN $TUTORIAL_GCP_SPANNER_SERVICE_INSTANCE_NAME -c '{"name":"trades"}'
+
+# The above command returns before the create has completed. Loop until the service creation has succeeded.
+until cf service $TUTORIAL_GCP_SPANNER_SERVICE_INSTANCE_NAME | grep "succeeded"; do
+  echo sleeping
+  sleep 1
+  if [[ $(cf service tutorial-gcp-spanner-service-instance | grep status) == *failed* ]]
+  then
+      echo "failed"
+      exit 1
+  fi
+done
 
