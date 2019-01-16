a702dcf0cadd82395e0aa9b77bdeacd41d209692
Steve Greenberg <spgreenberg@gmail.com>
2019-01-16 09:17:38 -0700

    create-bind-purge service instance

---
+++ simple-service-broker/ci/tasks/bind-service.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set +x
+set -e
+
+. cg-customer-broker/ci/tasks/common.sh
+
+cf bind-service ${BROKER_APPNAME} ${SERVICE_INSTANCE_NAME}
+
+instance=`cf services | grep ${SERVICE_INSTANCE_NAME} | grep ${BROKER_APPNAME}`
+
+if [ -z $instance ]; then
+  echo "Could not find bound service instance via 'cf services': App name = ${BROKER_APPNAME}, Service instance name = ${SERVICE_INSTANCE_NAME}"
+  exit 1
+fi
---
+++ simple-service-broker/ci/tasks/purge-service-instance.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set +x
+set -e
+
+. cg-customer-broker/ci/tasks/common.sh
+
+cf purge-service-instance -f ${SERVICE_INSTANCE_NAME}
+
+instance=`cf services | grep ${SERVICE_INSTANCE_NAME}`
+
+if [ ! -z $instance ]; then
+  echo "Service instance was not purged (found by cf services): Service instance name = ${SERVICE_INSTANCE_NAME}"
+  exit 1
+fi
