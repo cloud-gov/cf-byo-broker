1503315838278fb9d7bf497b159e11c6fce290fa
Steve Greenberg <spgreenberg@gmail.com>
2019-01-16 18:38:51 -0700

    wip - debug

--- simple-service-broker/ci/tasks/purge-service-instance.sh
+++ simple-service-broker/ci/tasks/purge-service-instance.sh
@@ -9,11 +9,7 @@ cf stop ${BROKER_APPNAME}
 
 cf purge-service-instance -f ${SERVICE_INSTANCE_NAME}
 
-instance="$(cf services | grep ${SERVICE_INSTANCE_NAME})"
-
-echo "instance: ${instance}"
-
-if [ -z "${instance}" ]; then
+if [ -z "$(cf services | grep ${SERVICE_INSTANCE_NAME})" ]; then
   echo "Service instance purged"
 else
   echo "Service instance was not purged (found by cf services): Service instance name = ${SERVICE_INSTANCE_NAME}"
