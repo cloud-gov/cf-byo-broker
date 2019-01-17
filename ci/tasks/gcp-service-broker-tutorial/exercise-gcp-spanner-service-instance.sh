fa15f4684c54a669e585341a539b0232257e2255
Steve Wall <steve.wall@primetimesoftware.com>
2019-01-17 13:17:59 -0700

    wip: Change where release artifact is housed from S3 to github release

---
+++ ci/tasks/gcp-service-broker-tutorial/exercise-gcp-spanner-service-instance.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+set -e
+
+. cg-customer-broker/ci/tasks/common.sh
+
+cd gcp-spanner-example-release
+
+unzip *.zip
+
+cf p $GCP_SPANNER_EXAMPLE_APP_NAME -p *.jar --no-start
+
+cf bs $GCP_SPANNER_EXAMPLE_APP_NAME $GCP_SPANNER_SERVICE -c `{"role":"spanner.databaseAdmin"}`
+
+cf start $GCP_SPANNER_EXAMPLE_APP_NAME
+
