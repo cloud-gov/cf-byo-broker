1ebd58e39aab504227445fefe19fbfefd17ed095
Steve Greenberg <spgreenberg@gmail.com>
2019-01-17 17:49:02 -0700

    use  command format

--- simple-service-broker/ci/tasks/create-service-broker.sh
+++ simple-service-broker/ci/tasks/create-service-broker.sh
@@ -5,6 +5,6 @@ set -e
 
 . cg-customer-broker/ci/tasks/common.sh
 
-broker_route=$(get_route $BROKER_APPNAME)
+broker_route="$(get_route $BROKER_APPNAME)"
 
-cf create-service-broker ${BROKER_APPNAME} ${BROKER_USERNAME} ${BROKER_PASSWORD} https://${broker_route} --space-scoped
+cf create-service-broker ${BROKER_APPNAME}-${CF_ORG}-${CF_SPACE} ${BROKER_USERNAME} ${BROKER_PASSWORD} https://${broker_route} --space-scoped
--- simple-service-broker/ci/tasks/service-brokers.sh
+++ simple-service-broker/ci/tasks/service-brokers.sh
@@ -5,9 +5,7 @@ set -e
 
 . cg-customer-broker/ci/tasks/common.sh
 
-broker_name=`cf service-brokers | grep ${BROKER_APPNAME}`
-
-if [ -z $broker_name ]; then
+if [ -z "$(cf service-brokers | grep ${BROKER_APPNAME})" ]; then
   echo "Could not find broker via 'cf service-brokers': Broker name = ${BROKER_APPNAME}"
   exit 1
 fi
