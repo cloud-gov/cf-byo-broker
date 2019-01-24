bae756e0f84df9b593e3d20d34b75690f1e02628
Steve Wall <steve.wall@primetimesoftware.com>
2019-01-24 05:42:59 -0700

    Change gcp service broker pipeline to reflect new zip structure.

--- gcp-service-broker/ci/tasks/exercise-trades-service-instance.sh
+++ gcp-service-broker/ci/tasks/exercise-trades-service-instance.sh
@@ -9,7 +9,8 @@ cd trades-release
 
 unzip *.zip
 
-cf p $TRADES_APP_NAME -p *.jar --no-start
+cd trades
+cf p $TRADES_APP_NAME -p trades*.jar --no-start --random-route
 
 cf bs $TRADES_APP_NAME $TUTORIAL_TRADES_SERVICE_INSTANCE_NAME -c '{"role":"spanner.databaseAdmin"}'
 
